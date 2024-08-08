79133c846b19085704ec4da108b6b7607d99317e
Audy Ruslan <4udyruslan@gmail.com>
2024-08-08 09:50:33 +0800

    Add & Update

--- kelola-akun.php
+++ kelola-akun.php
@@ -18,29 +18,37 @@
             <input type="text" class="form-control col-4 mb-3" id="nip" placeholder="Cari Pegawai...">
             </div>
             <table class="table">
-            <thead class="text-center" style="background-color: #4067A7; color: white;">
-                <tr>
-                <th>No</th>
-                <th>Nama Lengkap</th>
-                <th>Jabatan</th>
-                <th>Email</th>
-                <th>NIP/NIP DISTPH</th>
-                <th>Aksi</th>
-                </tr>
-            </thead>
-            <tbody class="text-center">
-                <tr>
-                <td>1</td>
-                <td>Fikran, S.Kom</td>
-                <td>Staff Kepegawaian</td>
-                <td>fikranagmail.com</td>
-                <td>19827327389272974</td>
-                <td>
-                    <div class="badge badge-pill badge-primary">Detail</div>
-                    <div class="badge badge-pill badge-danger">Reset</div>
-                </td>
-                </tr>
-            </tbody>
+                <thead class="text-center" style="background-color: #4067A7; color: white;">
+                    <tr>
+                    <th>No</th>
+                    <th>Nama Lengkap</th>
+                    <th>Jabatan</th>
+                    <th>Email</th>
+                    <th>NIP/NIP DISTPH</th>
+                    <th>Aksi</th>
+                    </tr>
+                </thead>
+                <tbody class="text-center">
+                    <?php 
+                    $no = 1;
+                    $sqlPegawai = mysqli_query($conn, "SELECT * FROM tb_pegawai
+                                                        INNER JOIN tb_jabatan
+                                                        ON tb_pegawai.jabatan_id = tb_jabatan.jabatan_id");
+                    while($row = mysqli_fetch_assoc($sqlPegawai)){
+                    ?>
+                        <tr>
+                            <td><?= $no++; ?>.</td>
+                            <td><?= $row["nama_lengkap"]; ?></td>
+                            <td><?= $row["nama_jabatan"]; ?></td>
+                            <td><?= $row["email_pegawai"]; ?></td>
+                            <td><?= $row["nip_pegawai"]; ?></td>
+                            <td>
+                                <a href="detail-pegawai.php?pegawai_id=<?= $row["pegawai_id"]; ?>" class="badge badge-pill badge-primary">Detail</a>
+                                <a href="#"  class="badge badge-pill badge-danger">Reset</a>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
             </table>
         </div>
         </div>
